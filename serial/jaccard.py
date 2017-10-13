b806fa44cbe723bc2f38846df043c4f2ed7b7691
Esteban Salazar Montoya <estebansm14@hotmail.com>
2017-10-13 18:36:47 -0500

    seguimos

--- serial/jaccard.py
+++ serial/jaccard.py
@@ -1,14 +1,14 @@
 from math import*
 import numpy as np
- 
+
 def jaccard_similarity(x,y):
     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-    print(intersection_cardinality)
+    #print(intersection_cardinality)
     union_cardinality = len(set.union(*[set(x), set(y)]))
-    print(list(set.union(*[set(x), set(y)])))
+    #print(list(set.union(*[set(x), set(y)])))
     return intersection_cardinality/float(union_cardinality)
 
-print jaccard_similarity([0,1,2],[0,2,3])
+#print jaccard_similarity([0,1,2],[0,2,3])
 
 # crear la matriz
 
