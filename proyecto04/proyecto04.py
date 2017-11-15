5ce90d256d2fda67bb3a01c992f2578f042a8d2d
Esteban Salazar Montoya <estebansm14@hotmail.com>
2017-11-15 10:38:26 -0500

    lectura de archivos

--- proyecto04/proyecto04.py
+++ proyecto04/proyecto04.py
@@ -1,28 +1,16 @@
-import pyspark
-from pypark.ml.clustering import KMeans
-from __future__ import print_function
-
-import sys
-
+import re
 from pyspark import SparkContext
-from pyspark.mllib.feature import Word2Vec
 
-USAGE = ("bin/spark-submit --driver-memory 4g "
-         "examples/src/main/python/mllib/word2vec.py text8_lines")
+# An even better cleanup would include stemming,
+# careful punctuation removal, etc.
+def clean(doc):
+    sc = SparkContext(appName="TFIDFExample")
+    return filter(lambda w: len(w) > 2,
+                  map(lambda s: s.lower(), re.split(r'\W+', doc)))
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print(USAGE)
-        sys.exit("Argument for file not provided")
-    file_path = sys.argv[1]
-    sc = SparkContext(appName='Word2Vec')
-    inp = sc.textFile(file_path).map(lambda row: row.split(" "))
-
-    word2vec = Word2Vec()
-    model = word2vec.fit(inp)
-
-    synonyms = model.findSynonyms('china', 40)
-
-    for word, cosine_distance in synonyms:
-        print("{}: {}".format(word, cosine_distance))
-    sc.stop()
+datasets = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/*.txt")    \
+           .mapValues(clean)                                            \
+           .cache()
+essayNames = datasets.map(lambda (filename, contents): filename).collect()
+docs = datasets.map(lambda (filename, contents): contents)
+print docs 
