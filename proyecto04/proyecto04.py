7ae81c638ca6a020f081af2c9a1a419e849be862
Esteban Salazar Montoya <estebansm14@hotmail.com>
2017-11-15 13:00:59 -0500

    tf idf

--- proyecto04/proyecto04.py
+++ proyecto04/proyecto04.py
@@ -1,18 +1,42 @@
-import re
+from _future_ import print_function
+
 from pyspark import SparkContext
+# $example on$                                                                                                                                                                    
+from pyspark.mllib.feature import HashingTF, IDF
+# $example off$                                                                                                                                                                   
 
-# An even better cleanup would include stemming,
-# careful punctuation removal, etc.
-sc = SparkContext(appName="TFIDFExample")
-def clean(doc):
-    
-    return filter(lambda w: len(w) > 2,
-                  map(lambda s: s.lower(), re.split(r'\W+', doc)))
+if _name_ == "_main_":
+    sc = SparkContext(appName="TFIDFExample")  # SparkContext                                                                                                                     
 
-datasets = sc.wholeTextFiles("hdfs:///datasets/gutenberg-txt-es/*.txt")    \
-           .mapValues(clean)                                            \
-           .cache()
-var = (filename, contents)
-essayNames = datasets.map(lambda var: filename).collect()
-docs = datasets.map(lambda var: contents)
-essayNames.saveAsTextFile("hdfs:///user/esalaza7/practica_out")
+    # $example on$                                                                                                                                                                
+    # Load documents (one per line).                                                                                                                                              
+    documents = sc.textFile("hdfs:///datasets/gutenberg-txt-es/12368.txt").map(lambda line: line.split(" "))
+
+    hashingTF = HashingTF()
+    tf = hashingTF.transform(documents)
+
+    # While applying HashingTF only needs a single pass to the data, applying IDF needs two passes:                                                                               
+    # First to compute the IDF vector and second to scale the term frequencies by IDF.                                                                                            
+    tf.cache()
+    idf = IDF().fit(tf)
+    tfidf = idf.transform(tf)
+
+    # spark.mllib's IDF implementation provides an option for ignoring terms                                                                                                      
+    # which occur in less than a minimum number of documents.                                                                                                                     
+    # In such cases, the IDF for these terms is set to 0.                                                                                                                         
+    # This feature can be used by passing the minDocFreq value to the IDF constructor.                                                                                            
+    idfIgnore = IDF(minDocFreq=2).fit(tf)
+    tfidfIgnore = idfIgnore.transform(tf)
+    # $example off$                                                                                                                                                               
+
+    print("tfidf:")
+    carpeta = open("tfidf.txt","w")
+    for each in tfidf.collect():
+        print(each)
+        carpeta.write(str(each))
+
+    print("tfidfIgnore:")
+    for each in tfidfIgnore.collect():
+        print(each)
+
+    sc.stop()
